c2ddc20b46bc83d9b0a56d829518527a0eaadbaa
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-07-14 22:11:50 +0200

    separate handlers

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,33 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { getAllContacts, addContact, deleteContact } from './operations';
-
-const handlePending = state => {
-  state.isLoading = true;
-};
-
-const handleRejected = (state, action) => {
-  state.isLoading = false;
-  state.error = action.payload;
-};
-
-const handleGetAllContactsFulfilled = (state, action) => {
-  state.isLoading = false;
-  state.error = null;
-  state.items = action.payload;
-};
-
-const handleAddContactFulfilled = (state, action) => {
-  state.isLoading = false;
-  state.error = null;
-  state.items.push(action.payload);
-};
-
-const handleDeleteContactFulfilled = (state, action) => {
-  state.isLoading = false;
-  state.error = null;
-  const index = state.items.findIndex(item => item.id === action.payload.id);
-  state.items.splice(index, 1);
-};
+import {
+  handleAddContactFulfilled,
+  handleDeleteContactFulfilled,
+  handleGetAllContactsFulfilled,
+  handlePending,
+  handleRejected,
+} from './handlers';
 
 const contactsSlice = createSlice({
   name: 'contacts',
